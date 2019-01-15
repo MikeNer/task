8d6832a5f2509549c8ae547e3869ee75282f4019
Michael Klinger <michael.klinger@htwg-konstanz.de>
2019-01-15 01:08:45 +0100

    Js Check Form Input

--- aufgabe1/js/main.js
+++ aufgabe1/js/main.js
@@ -1,5 +1,16 @@
 function checkForm(){   
-    alert("Wir haben deine Nachricht erhalten");
-    var errorMessage="";
+    var inputs = document.getElementById("seminarForm").elements;
+    var x;
     
+    for (i = 0; i < inputs.length; i++) {
+        if(inputs[i].value.length==0){
+            alert("Bitte alle Felder ausfüllen");
+            return
+        }else{
+            alert("Wir haben deine Nachricht erhalten");
+        }
+      
+      }
+  
+
 }
